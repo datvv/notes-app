fe3c518c795d6670e5c866e2eb52680f847a6695
datvv <datvv>
2022-09-08 21:02:34 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="m-6 text-[30px] text-center">React</div>
+    </div>
+  );
+}
+
+export default App;
