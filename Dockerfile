ce9ea09cb5697b264dca0578cca3707b708ec4f6
datvv <datvv>
2022-09-19 13:20:38 +0700

    CICD: update build CICD with Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# build stage
+FROM node:18 as build-stage
+
+ENV HOME /opt/src/app
+WORKDIR $HOME
+
+COPY ["package.json", "yarn.lock", "./"]
+RUN yarn install
+
+COPY . .
+RUN yarn install --force
+RUN yarn build
+
+# production stage
+FROM nginxinc/nginx-unprivileged:stable-alpine as production-stage
+COPY --from=build-stage /opt/src/app/dist /usr/share/nginx/html
+COPY default.conf /etc/nginx/conf.d/default.conf
+
+CMD sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
--- src/components/NoteEditor.jsx
+++ src/components/NoteEditor.jsx
@@ -42,7 +42,7 @@ const NoteEditor = () => {
   };
 
   const handleInput = function (data) {
-    setNoteInput(data);
+    setNoteInput("<div>" + data + "</div>");
     console.log("AA :", data);
   };
 
@@ -64,6 +64,15 @@ const NoteEditor = () => {
           </h3>
 
           <div className="px-4">
+            <textarea
+              type="text"
+              rows={8}
+              placeholder="Enter data note"
+              className="text-2xl font-bold bg-gray-50  text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-3"
+              value={noteInput}
+              onChange={(e) => handleInput(e.target.value)}
+            />
+
             <input
               type="text"
               placeholder="Enter title"
@@ -71,6 +80,7 @@ const NoteEditor = () => {
               value={title}
               onChange={(e) => handleUpdateCurrentNote(e.target.value, "title")}
             />
+
             <div className="border border-t-1"></div>
             <textarea
               type="text"
